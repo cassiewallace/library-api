44de4ef36159e6e734d4e284d266f97f4157d24a
Cassie Wallace <cassbwallace@gmail.com>
2019-09-27 18:47:24 -0600

    Fix post permissions

--- books/permissions.py
+++ books/permissions.py
@@ -6,11 +6,5 @@ class IsStaffOrReadOnly(permissions.BasePermission):
     Custom permission to allow only staff to create and edit books.
     """
 
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        elif request.user.is_staff:
-            return True
-
-        return False
+    def has_permission(self, request, view):
+        return request.user.is_staff or request.method in permissions.SAFE_METHODS
