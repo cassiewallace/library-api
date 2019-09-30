7c282f35358f4fe4cfec31da94eb6eef78f7005e
Cassie Wallace <cassbwallace@gmail.com>
2019-09-30 15:11:32 -0600

    Add custom endpoints for check out and in

--- books/views.py
+++ books/views.py
@@ -1,8 +1,11 @@
+from rest_framework import status, viewsets
+from rest_framework.decorators import action, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from books.models import Book
 from books.permissions import IsStaffOrReadOnly
 from books.serializers import BookSerializer
-from rest_framework import viewsets
-from rest_framework.decorators import permission_classes
 
 
 @permission_classes([IsStaffOrReadOnly])
@@ -16,3 +19,25 @@ class BookViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=True, methods=['post'], 
+        permission_classes=[IsAuthenticated], name='Check Out')
+    def check_out(self, request, pk=None):
+        """Check out a book."""
+        book = self.get_object()
+
+        book.checked_out_by = request.user
+        book.save()
+
+        return Response({'status': 'checked out'})
+
+    @action(detail=True, methods=['post'], 
+        permission_classes=[IsAuthenticated], name='Check In')
+    def check_in(self, request, pk=None):
+        """Check in a book."""
+        book = self.get_object()
+
+        book.checked_out_by = None
+        book.save()
+
+        return Response({'status': 'checked back in'})
