20512d439ff6d001b4acfa256a3e4f1a939ab932
Cassie Wallace <cassbwallace@gmail.com>
2019-10-02 15:36:08 -0600

    Fix order of imports (#8)

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User, Group
+
 from rest_framework import serializers
+
 from books.models import Book
 
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User, Group
-from users.serializers import UserSerializer, GroupSerializer
+
 from rest_framework import permissions, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from users.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
