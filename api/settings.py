ea551676d59a7a0e7dc0bd7e0555846d1ccfba4f
Cassie Wallace <cassbwallace@gmail.com>
2019-10-03 15:18:13 -0600

    Add static file directories (#9)

--- api/settings.py
+++ api/settings.py
@@ -105,7 +105,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'api.pagination.PageNumberPaginationDataOnly',
